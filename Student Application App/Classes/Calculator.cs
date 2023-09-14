e67ec7293374d4008fa0203205039cfac0ddadf4
Elin Jansson <99elinjansson@gmail.com>
2023-09-14 21:47:05 +0200

    Add project files.

---
+++ Student Application App/Classes/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Student_Application_App.Classes
+{
+    public class Calculator
+    {
+        private string Output(double x, double y, string symbol, double result)
+        {
+            return $"{x} {symbol} {y} = {result}";
+        }
+        public  void Add(double x, double y, out string result) => result = Output(x,y,"+", (x+y));
+        public double Add(double x, double y) => x + y;
+
+        public  void Subtract (double x, double y, out string result) => result = Output(x,y,"-", (x-y));
+        public double Subtract(double x, double y) => x - y;
+
+        public  void Multiply (double x, double y, out string result) => result = Output(x, y, "x", (x*y));
+        public double Multiply(double x, double y) => x * y;
+
+        public  void Divide (double x, double y, out string result) => result = Output(x,y,"/",(x/y));
+        public double Divide(double x, double y) => x / y;
+
+    }
+}
---
+++ Student Application App/Classes/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace Student_Application_App.Classes
+{
+    public class Student
+    {
+
+        private readonly string _firstName = string.Empty;
+        private readonly string _lastName = string.Empty;
+        public string FullName => $"{_firstName} {_lastName}";
+        public readonly int Age = default;
+
+        public Student() { }
+        public Student(string firstName, string lastName, int age) => (_firstName, _lastName, Age) = (firstName, lastName, age);
+
+
+    }
+}
